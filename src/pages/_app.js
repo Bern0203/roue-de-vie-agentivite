b57ca0605e6cbd09aa95d072085d601b947a268e
bernard challiol <bernardchalliol@gmail.com>
2025-01-29 19:41:10 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,21 +1,5 @@
 import '../styles/globals.css'
-import { Playfair_Display, Roboto, Montserrat } from 'next/font/google'
-
-const playfair = Playfair_Display({ subsets: ['latin'] })
-const roboto = Roboto({ weight: ['400', '700'], subsets: ['latin'] })
-const montserrat = Montserrat({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <style jsx global>{`
-        :root {
-          --font-playfair: ${playfair.style.fontFamily};
-          --font-roboto: ${roboto.style.fontFamily};
-          --font-montserrat: ${montserrat.style.fontFamily};
-        }
-      `}</style>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
